a42b76b73d053f3481f71e7342dbfe5279bd12d7
John <johnmulvey48@yahoo.com>
2022-09-23 12:24:44 -0400

    git@github.com:Jmulvez/phase-3-sinatra-with-active-record-get-lab.git

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
-class ApplicationController < Sinatra::Base
-
-  # add routes
-
+class ApplicationController
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+  get '/bakeries:id' do
+    bakeries = Bakery.find(id)
+  end
+  get '/baked_goods/by_price' do
+    bakeries = Bakery.all
+  end
+  get '/baked_goods/most_expensive' do
+    bakeries = Bakery.maximum(price)
+  end
 end
